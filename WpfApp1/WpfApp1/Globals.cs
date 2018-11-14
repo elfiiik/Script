9098052fa6176fe5bac6fe1f52ba8579b8a87ac8
punaja16 <punaja16@DST20705>
2018-11-14 15:11:29 +0100

    YEET

---
+++ WpfApp1/WpfApp1/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Globals
+    {
+        public int Strenght { get; set; }
+        public int HP { get; set; }
+        public int Intel { get; set; }
+    }
+}
